8c427253326208b2d8cb77e1606b01ec726cc6e4
Nhoj <nhojplummer@yahoo.com>
2023-05-23 11:52:22 -0400

    Setting up test array for checklist

--- checklist/src/components/checklist/Checklist.js
+++ checklist/src/components/checklist/Checklist.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react"
 
-function Checklist() { 
+function Checklist() {
   const checklist = ["Apple", "Banana", "Tea", "Coffee"]
   return (
-  
-
-    <div className='checkList'></div>
+    <div className="checkList">
+      <div className="list-container">
+        {checklist.map((item, index) => (
+          <div key={index}>
+            <span>{item}</span>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
