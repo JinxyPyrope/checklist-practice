dd08baa282dbafdd96d6f1de0fd0f74e013754ea
Nhoj <nhojplummer@yahoo.com>
2023-05-23 12:05:21 -0400

    Testing out possibly solution

--- checklist/src/components/checklist/Checklist.js
+++ checklist/src/components/checklist/Checklist.js
@@ -1,12 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
 
 function Checklist() {
   const checklist = ["Apple", "Banana", "Tea", "Coffee"]
+
+  const [checked, setChecked] = useState([])
+
+  const updatedList = e => {
+    const updatedList = [...checked]
+
+    if (e.target.checked) {
+      updatedList = [...checked, e.target.value]
+    } else {
+      updatedList.splice(checked.indexOf(e.target.value), 1)
+    }
+
+    setChecked(updatedList)
+  }
+
   return (
     <div className="checkList">
       <div className="list-container">
         {checklist.map((item, index) => (
           <div key={index}>
+            <input value={item} type="checkbox" />
             <span>{item}</span>
           </div>
         ))}
